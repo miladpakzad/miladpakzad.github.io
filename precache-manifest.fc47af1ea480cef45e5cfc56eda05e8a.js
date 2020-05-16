4d1f57f366d50a56f519b2c3bf53fc4617e3cb9a
milad pakzad <miladpakzad8888@gmail.com>
2020-05-16 16:30:47 +0430

    Init Commit

---
+++ precache-manifest.fc47af1ea480cef45e5cfc56eda05e8a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a9d1c0619cc15a4876",
+    "url": "/css/chunk-vendors.c1796b2e.css"
+  },
+  {
+    "revision": "7088d4d0db7a1c6ac514ed4f1e5d1d5b",
+    "url": "/img/app_icons/icon-128x128.png"
+  },
+  {
+    "revision": "7088d4d0db7a1c6ac514ed4f1e5d1d5b",
+    "url": "/img/app_icons/icon-144x144.png"
+  },
+  {
+    "revision": "7088d4d0db7a1c6ac514ed4f1e5d1d5b",
+    "url": "/img/app_icons/icon-152x152.png"
+  },
+  {
+    "revision": "569b1e3c63cfbfe0bde496858113a6c2",
+    "url": "/img/app_icons/icon-16x16.png"
+  },
+  {
+    "revision": "7088d4d0db7a1c6ac514ed4f1e5d1d5b",
+    "url": "/img/app_icons/icon-192x192.png"
+  },
+  {
+    "revision": "01d1c86f376198656dc0fa9f0b5cf1da",
+    "url": "/img/app_icons/icon-32x32.png"
+  },
+  {
+    "revision": "7088d4d0db7a1c6ac514ed4f1e5d1d5b",
+    "url": "/img/app_icons/icon-384x384.png"
+  },
+  {
+    "revision": "7088d4d0db7a1c6ac514ed4f1e5d1d5b",
+    "url": "/img/app_icons/icon-512x512.png"
+  },
+  {
+    "revision": "c7d914edca3d93e6aeb67a315ce82d82",
+    "url": "/img/app_icons/icon-72x72.png"
+  },
+  {
+    "revision": "2a571dcf066cc24153f3f1651dc57904",
+    "url": "/img/app_icons/icon-96x96.png"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "543c8ee96a6d65289b0517fe5e7f9472",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae3cb4f53c636604f2d7",
+    "url": "/js/about.353c4158.js"
+  },
+  {
+    "revision": "8941abe224d0b3a3f43f",
+    "url": "/js/app.65a8a94c.js"
+  },
+  {
+    "revision": "71a9d1c0619cc15a4876",
+    "url": "/js/chunk-vendors.b33a612f.js"
+  },
+  {
+    "revision": "5f209e35bfd003e15c743203dac7a79b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
